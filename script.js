a43cc17314df2ba1ce060968ce0fd5339f3a4a97
Meghana17179 <meghanachalavadi7@gmail.com>
2024-10-19 05:55:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let result = document.getElementById("result");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.value == "=")
+        {
+            try {
+                result.value = eval(result.value);
+            } catch (error) {
+                result.value = "Error";
+            }
+        } 
+        else if (button.value == "C")
+        {
+            result.value = "";
+        }
+        else 
+        {
+            result.value += button.value;
+        }
+    });
+});
